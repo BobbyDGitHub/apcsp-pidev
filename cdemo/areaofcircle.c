db991b5039b0ef256110440b8c8f329ecb5558f5
BobbyDGitHub <50212178+BobbyDGitHub@users.noreply.github.com>
2019-05-27 20:22:55 -0700

    Create areaofcircle.c

---
+++ cdemo/areaofcircle.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int areaOfCircle(float r)
+{
+return (3.1415*r*r);
+}
+
+int main()
+{
+
+float r;
+float area;
+area = areaOfCircle(r);
+for(r = 3; r < 13; r = r +1)
+{
+  float d = r + 0.5;
+  float e;
+  e = areaOfCircle(d);
+printf("Area: %f\n",e);
+
+  }
+  return 0;
+  }
